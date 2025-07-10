6016c1930287e7668f608054626c4a5e3083a672
Caio Poleza <caio_p_santos@estudante.sesisenai.org.br>
2025-07-10 10:12:49 -0300

    add

---
+++ exercicio02.php
@@ -0,0 +1,46 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcular a tabuada de um número.</title>
+</head>
+<body>
+
+   <form method="POST" action="">
+        <label for="Tabuada">Calcular a tabuada:</label>
+        <input type="number" id="Tabuada" name="Tabuada" required>
+        <button type="submit" name="Tabuada">Verificar</button>
+   </form>
+
+   <?php
+   
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    if(isset($_POST['Tabuada'])){
+        $numero = $_POST['Tabuada'];
+          * 0++
+        if($numero % 2 == 0){
+         echo "O número é Par $numero";
+
+        }else{    
+
+            for($Tabuad = $numero  $i++){
+        
+            if($numero % 2 <> 0);{
+                
+                    $ehImpar = false;
+                    
+                    echo "O número é Impar $numero ";
+
+                };
+            };
+         
+        };
+    };
+
+   
+
+   ?>
+ 
+</body>
+</html>
